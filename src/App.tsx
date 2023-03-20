39a71c8e85d1f048a4759e133f4fd6d8246c32b7
222 <kiravishn9@gmail.com>
2023-03-20 00:50:52 -0400

    fix: eslint and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './pages/Main';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Error } from './pages/404';
 import './App.css';
 import About from './pages/About';
@@ -9,11 +9,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="home" element={<Main />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Route path="home" element={<Main />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} />
       </BrowserRouter>
     </div>
   );
