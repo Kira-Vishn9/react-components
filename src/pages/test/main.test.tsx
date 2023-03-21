c746522ffe163f4c31cf5e4aed6e2ddece52edd1
222 <kiravishn9@gmail.com>
2023-03-21 14:03:49 -0400

    fix: navbar

--- src/pages/test/main.test.tsx
+++ src/pages/test/main.test.tsx
@@ -1,12 +1,8 @@
-import { render } from '@testing-library/react';
-import Main from '../Main';
 import JSONFILE from '../../../cards.json';
 
 describe('Home', () => {
   test('Проверка на количество, Карточек', () => {
-    const keys = Object.keys(JSONFILE);
-    const { container } = render(<Main />);
-    const cards = container.querySelectorAll('.main');
-    expect(1).toEqual(1);
+    const count = Object.keys(JSONFILE).length;
+    expect(count).toEqual(4);
   });
 });
